c04a3047af955e732fcda225fcf47ed0542cf3bb
moseiei132 <moseiei132@gmail.com>
2021-06-11 17:17:34 +0700

    add register user exists

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserRepository } from "../users/repositories/user.repository";
+import { UserService } from "../users/services/user.service";
+import { UserModule } from "../users/user.module";
 import { AuthController } from "./auth.controller";
 import { JwtStrategy } from "./jwt.strategy";
 import { AuthService } from "./services/auth.service";
@@ -13,6 +15,7 @@ import { AuthService } from "./services/auth.service";
         TypeOrmModule.forFeature([UserRepository]),
         PassportModule,
         ConfigModule,
+        UserModule,
         JwtModule.registerAsync({
             imports: [ConfigModule],
             useFactory: (configService: ConfigService) => ({
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserRepository } from "../../users/repositories/user.repository";
@@ -6,6 +6,7 @@ import { IAccessToken, ILogin, IRegister } from "../interfaces/auth.interface";
 import * as bcrypt from 'bcrypt'
 import { TUser } from "../../users/transformers/user.transformer";
 import { plainToClass } from "class-transformer";
+import { UserService } from "../../users/services/user.service";
 
 @Injectable()
 export class AuthService {
@@ -13,22 +14,27 @@ export class AuthService {
         private jwtService: JwtService,
         @InjectRepository(UserRepository)
         private userRepo: UserRepository,
-    ){}
+        private userService: UserService
+    ) { }
     async login(data: ILogin): Promise<IAccessToken> {
         const user = await this.userRepo.findOne({ username: data.username })
         if (!user) throw new UnauthorizedException('User does not exist')
         const isMatch = await bcrypt.compare(data.password, user.password)
         if (!isMatch) throw new UnauthorizedException('Invalid password')
         return {
-          accessToken: this.jwtService.sign({
-            id: user.id,
-            username: data.username,
-          }),
+            accessToken: this.jwtService.sign({
+                id: user.id,
+                username: data.username,
+            }),
         }
-      }
-    
-      async register(userData: IRegister): Promise<TUser> {
+    }
+
+    async register(userData: IRegister): Promise<TUser> {
+        let user = await this.userService.getUserByUsername(userData.username)
+        if (user) throw new UnprocessableEntityException('Username already exists')
+        user = await this.userService.getUserByEmail(userData.email)
+        if (user) throw new UnprocessableEntityException('Email already exists')
         userData.password = await bcrypt.hash(userData.password, 10)
-        return plainToClass(TUser, await this.userRepo.save(userData)) 
-      }
+        return plainToClass(TUser, await this.userRepo.save(userData))
+    }
 }
