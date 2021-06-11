c2cf5506120dd16f300bbdb737883893c6a70fff
moseiei132 <moseiei132@gmail.com>
2021-06-11 16:03:47 +0700

    add repositories

---
+++ src/comments/repositories/comment-reaction.repository.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from 'typeorm'
+import { CommentReaction } from '../entities/comment-reaction.entity';
+
+
+@EntityRepository(CommentReaction)
+export class CommentReactionRepository extends Repository<CommentReaction> {}
